2813bdcc9740c1234b54eb059bae2c1aab7cbf86
FlowerSeerat <seerat3208@gmail.com>
2021-07-30 08:17:47 -0400

    added constraint

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var log6;
+var chain;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -15,7 +17,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+    log6 = new Log(230,180,80,PI/2);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -36,6 +38,7 @@ function setup(){
 
     bird = new Bird(100,100);
 
+     chain= new Chain(log6.body,bird.body);
 }
 
 function draw(){
@@ -61,4 +64,8 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
+    chain.display();
+
+    
 }
